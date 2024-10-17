061b07118dc0ff61fcaff5ce34855bbf8fb5bc51
Abdelali Yassir <94761225+Ayassir05@users.noreply.github.com>
2024-10-17 11:44:33 +0100

    first commit

--- Days/Shell00/ex04/Solution.sh
+++ Days/Shell00/ex04/Solution.sh
@@ -3,6 +3,6 @@ ls -tmp
 
 -let's break that down:                                                                                                               '
 ls -tmp Explanation:
-+++ -t: Sorts files by modification time (most recent first).
+'+++ -t: Sorts files by modification time (most recent first).
 +++ -m: Outputs the list in a comma-separated format.
-+++ -p: Appends a / after directory names.
++++ -p: Appends a / after directory names.'
