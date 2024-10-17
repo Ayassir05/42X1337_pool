3d007e003bc22db990c131b695969bc1fb6649d8
Abdelali Yassir <94761225+Ayassir05@users.noreply.github.com>
2024-10-17 11:42:09 +0100

    first commit

---
+++ Days/Shell00/ex04/Solution.sh
@@ -0,0 +1,8 @@
+touch midLS
+ls -tmp
+
+-let's break that down:                                                                                                               '
+ls -tmp Explanation:
++++ -t: Sorts files by modification time (most recent first).
++++ -m: Outputs the list in a comma-separated format.
++++ -p: Appends a / after directory names.
