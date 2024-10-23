a54c77e1a613c2f48bf201c50c466eab0e9c67bf
Abdelali Yassir <94761225+Ayassir05@users.noreply.github.com>
2024-10-23 07:08:28 +0100

    Update Solution.sh

--- Days/Shell00/ex07/Solution.sh
+++ Days/Shell00/ex07/Solution.sh
@@ -1,1 +1,5 @@
-"Firstly, the exercise provides a file named resources.tar.gz as a resource. We need to download it (i'll put the file in Days/Shell00/ex07/resource)and extract its contents."
+"Firstly, the exercise provides a file named resources.tar.gz as a resource (i'll put the file in Days/Shell00/ex07/resources.tar.gz). We need to download it and extract its contents."
+cp ~/Downloads/resources.tar.gz .
+tar -xf resources.tar.gz
+patch a -o b < sw.diff
+diff a b > sw.diff
